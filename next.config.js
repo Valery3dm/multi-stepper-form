a87444394ab5aaaf3e2aa67a171730ef0677a6db
Valery <ilchenko.valery.developer@gmail.com>
2023-05-18 15:09:45 +0300

    next config added images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: ' valery3dm.github.io',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
